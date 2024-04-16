3d4ce6d224b5f7dc628a95aee79cc083a401fbca
Lewis <lewis@anystay.com.au>
2024-04-16 13:20:08 +1000

    improve typescript types

--- src/app/api/batch/route.ts
+++ src/app/api/batch/route.ts
@@ -3,15 +3,29 @@ import {NextResponse} from 'next/server';
 import type {NextRequest} from 'next/server';
 import { nanoid } from 'nanoid';
 
-export async function POST(req: NextRequest, res: NextResponse) {
+interface Batch {
+    model: string;
+    quantity: number;
+    licenseLevel: number;
+    date: string;
+    comment ?: string;
+}
+
+interface MachineNumbers {
+    serial_number: string;
+    batch_id: number;
+}
+
+export async function POST(req: NextRequest) {
     const body = await req.json();
-    const {batch} = body;
+    // const {batch} = body;
+    const batch: Batch = body.batch;
     const quantity: number = batch.quantity
     const prisma = new PrismaClient()
     const result = await prisma.batches.create({
         data: batch
     })
-    const machineNumbers = [];
+    const machineNumbers: MachineNumbers[] = [];
     for (let i = 0; i < quantity; i++) {
         machineNumbers.push({
             serial_number: nanoid(),
